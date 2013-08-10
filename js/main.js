f56dacea0b128f6571a315ba2f45b2441365b09b
Pascal Pfiffner <phase.of.matter@gmail.com>
2013-08-10 01:00:43 -0400

    Phone and tablet sized versions
    Not yet finished: not all animations work properly and the hover must be brought back.

--- js/main.js
+++ js/main.js
@@ -2,105 +2,117 @@
  *  Main script file.
  */
 
+var _w_tablet = 460;
+var _w_desktop = 980;
 
 /**
  *  Sets up our circus.
  */
 function setupCircus() {
+	var parent = $('#circus');
 	
-	// circus logo
-	$('#circus-logo').attr('src', onRetinaScreen() ? 'images/Ossus-300@2x.png' : 'images/Ossus-300.png').hide();
-	
-	// circus
-	var kids = $('#circus').find('.item');
-	var step = 360 / kids.length;
-	
-	kids.each(function(i, item) {
-		var view = $(item);
-		var app = view.data('app');
-		var d = app in _data ? _data[app] : {};
-		var app_name = 'name' in d ? d['name'] : app;
+	for (p in _data) {
+		var app = _data[p];
+		var app_name = 'name' in app ? app['name'] : p;
+		var li = $('<li/>');
 		
 		// icon & link
-		var url = 'url' in d ? d['url'] : '';
+		var url = 'url' in app ? app['url'] : '';
 		var link = $('<a/>').attr('href', url);
-		var icon = $('<img/>').attr('src', 'images/' + app + (onRetinaScreen() ? '@2x' : '') + '.png').attr('alt', app);
+		var icon = $('<img/>').attr('src', 'images/' + p + (onRetinaScreen() ? '@2x' : '') + '.png').attr('alt', p);
 		var span = $('<span/>').text(app_name);
 		link.append(icon).append(span);
-		view.append(link);
+		li.append(link);
 		link.click(function(e) {
 			e.stopPropagation();
 			return true;
 		});
 		
 		// hover
-		var desc = 'desc' in d ? d['desc'] : null;
+		var desc = 'desc' in app ? app['desc'] : null;
 		if (desc) {
 			var hover = $('<div/>').html('<h2>' + app_name + '</h2>' + desc);
 			hover.click(function(e) {
 				e.stopPropagation();
 				e.preventDefault();
 			});
-			view.append(hover);
+			li.append(hover);
 		}
 		
 		// position
-		view.show();
-		circlePutItemToPositionAtAngle(view, 90);
-	});
+		parent.append(li);
+		
+		if ($(window).width() >= _w_desktop) {
+			circlePutItemToPositionAtAngle(li, 90);
+		}
+	}
 }
 
 
 /**
  *  Toggles the circus state.
  */
-var _circus_timeout = null;
-
-// setup click function
 function toggleCircus() {
+	var winwidth = $(window).width();
+	
+	var main = $('#full');
 	var circus = $('#circus');
-	if (_circus_timeout) {
-		window.clearTimeout(_circus_timeout);
+	circus.parent().show();
+	
+	// phones & tablets
+	if (winwidth < _w_desktop) {
+		if (main.hasClass('shown')) {
+			hideCircusOnPhones();
+		}
+		else {
+			showCircusOnPhones();
+		}
+		return;
 	}
 	
-	var kids = circus.find('.item');
+	// desktop
+	var kids = circus.find('li');
 	var step = 360 / kids.length;
 	
-	// default vars when showing the circus
-	var target_size = '';
-	var percent_timeout = 1000;
 	var first_angle = -110;
-	var show_circus = true;
+	var show_circus = !main.hasClass('shown');
 	
 	// we are hiding the circus
-	if (circus.hasClass('shown')) {
+	if (!show_circus) {
 		step = 0;
-		target_size = '40%';
-		percent_timeout = 0;
 		first_angle = 90;
-		show_circus = false;
 	}
 	
 	// rotate circus
-	
 	kids.each(function(i, item) {
 		circleItemTo($(item), first_angle + i * step, circus);
 	});
-	circus.toggleClass('shown');
+	main.toggleClass('shown');
 	
 	// set background size - must be set in percent so the CSS animation kicks in
-	var percent_300 = 300 / $(window).width() * 100;
-	$('#full').css('background-size', percent_300 + '%');//.prop('onclick', null);
-	_circus_timeout = window.setTimeout(function() {
-		if (show_circus) {
-			$('#full').css('background-size', target_size).css('background-image', 'none');
-			$('#circus-logo').show();
-		}
-		else {
-			$('#full').css('background-size', target_size).css('background-image', 'url(images/Ossus-1200.png)');
-			$('#circus-logo').hide();
-		}
-	}, percent_timeout);
+	if (show_circus) {
+		var percent_300 = 300 / $(window).width() * 100;
+		$('#full').css('background-size', percent_300 + '%');
+	}
+	else {
+		$('#full').css('background-size', '40%');
+	}
+}
+
+function showCircusOnPhones() {
+	$('#circus-wrap').show();
+	
+	window.setTimeout(function() {
+		$('#full').addClass('shown');
+	}, 10);
+}
+
+function hideCircusOnPhones() {
+	$('#full').removeClass('shown');
+	
+	window.setTimeout(function() {
+		$('#circus-wrap').hide();
+	}, 1000);
 }
 
 
